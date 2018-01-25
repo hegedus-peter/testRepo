7fd30cce8254b148de1c4a86b5b43a0904948186
Hegedűs Péter <hegedusp@Allround.net>
2018-01-25 15:04:31 +0100

    Add jenkinsProject

---
+++ jenkinsProject/src/main/java/net/allround/tapsol/collator/jenkinsproject/Main.java
@@ -0,0 +1,18 @@
+package net.allround.tapsol.collator.jenkinsproject;
+
+/**
+ *
+ * @author hegedusp
+ */
+public class Main {
+    
+    public static double divide(double a, double b) //return a/b
+    {
+        return a/b;
+    }    
+    public static void main(String[] args) {
+       double retVal = divide(1,2);
+        System.out.println("The result: " + retVal );
+    }
+
+}
