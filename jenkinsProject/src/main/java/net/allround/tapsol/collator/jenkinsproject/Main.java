fc9dc9ecdbfe2c2b2ee18de9d964f7395a9f031c
Hegedűs Péter <hegedusp@Allround.net>
2018-01-31 16:29:42 +0100

    Add add function

--- jenkinsProject/src/main/java/net/allround/tapsol/collator/jenkinsproject/Main.java
+++ jenkinsProject/src/main/java/net/allround/tapsol/collator/jenkinsproject/Main.java
@@ -12,8 +12,15 @@ public class Main {
             throw new ArithmeticException();
         return a/b;
     }    
+    
+     public static double add(double a, double b) //return a+b
+    {
+        return a+b;
+    } 
+    
     public static void main(String[] args) {
        double retVal = divide(1,0);
+       double add = add(1,2);
         System.out.println("The result: " + retVal );
     }
 
