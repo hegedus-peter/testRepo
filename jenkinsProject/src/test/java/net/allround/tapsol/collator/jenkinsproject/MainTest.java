d3cca4a52b18cb5dcbce4053749e0cdf14a1d7ff
Hegedűs Péter <hegedusp@Allround.net>
2018-01-31 16:27:10 +0100

    Rework tests

--- jenkinsProject/src/test/java/net/allround/tapsol/collator/jenkinsproject/MainTest.java
+++ jenkinsProject/src/test/java/net/allround/tapsol/collator/jenkinsproject/MainTest.java
@@ -17,6 +17,11 @@ public class MainTest {
         assertEquals(0.5,Main.divide(1, 2),0);
     }
     
+    @Test
+    public void testDivideGood2() {
+        assertEquals(1,Main.divide(1, 1),0);
+    }
+    
     @Test (expected = ArithmeticException.class)
     public void testDivideBad() {
         Main.divide(2,0);
