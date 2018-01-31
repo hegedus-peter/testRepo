861d3ba69ceb82214a391deaa13954bf82900085
Hegedűs Péter <hegedusp@Allround.net>
2018-01-31 16:22:24 +0100

    Add test5

--- jenkinsProject/src/test/java/net/allround/tapsol/collator/jenkinsproject/MainTest.java
+++ jenkinsProject/src/test/java/net/allround/tapsol/collator/jenkinsproject/MainTest.java
@@ -17,26 +17,6 @@ public class MainTest {
         assertEquals(0.5,Main.divide(1, 2),0);
     }
     
-    @Test
-    public void testDivideGood_2() {
-        assertEquals(0.5,Main.divide(12.23,214.22),0);
-    }
-    
-    @Test
-    public void testDivideGood_3() {
-        assertEquals(0.5,Main.divide(21312.23,2),0);
-    }
-    
-    @Test
-    public void testDivideGood_4() {
-        assertEquals(0.5,Main.divide(999.23,23),0);
-    }
-    
-    @Test
-    public void testDivideGood_5() {
-        assertEquals(0.5,Main.divide(992349.233,23),0);
-    }
-    
     @Test (expected = ArithmeticException.class)
     public void testDivideBad() {
         Main.divide(2,0);
